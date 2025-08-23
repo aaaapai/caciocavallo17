1ead21f17317cede96abaee308e56be129f14571
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-08-23 20:50:20 +0800

    Update build_and_out.sh

--- build_and_out.sh
+++ build_and_out.sh
@@ -5,5 +5,5 @@ mvn -B package --file pom.xml -DskipTests
 
 rm -rf out
 mkdir -p out
-cp cacio-*/target/cacio-*-1.19.2-SNAPSHOT.jar out
-mv out/cacio-agent-1.19.2-SNAPSHOT.jar out/cacio-agent.jar
+cp cacio-*/target/cacio-*-1.19.1-SNAPSHOT.jar out
+mv out/cacio-agent-1.19.1-SNAPSHOT.jar out/cacio-agent.jar
