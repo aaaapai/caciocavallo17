78836740ab17a033647ba1be8d121ea8ed91d6f8
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-10-05 19:01:19 +0800

    Update build_and_out.sh

--- build_and_out.sh
+++ build_and_out.sh
@@ -5,9 +5,10 @@ mvn -B package --file pom.xml -DskipTests
 
 rm -rf out
 mkdir -p out
-cp cacio-*/target/cacio-*-1.19.1-SNAPSHOT.jar out
-mv out/cacio-agent-1.19.1-SNAPSHOT.jar out/cacio-agent.jar
+cp cacio-*/target/cacio-*-1.19.2-SNAPSHOT.jar out
+mv out/cacio-agent-1.19.2-SNAPSHOT.jar out/cacio-agent.jar
 
+<< EOF
 # 下载专门的字节码工具
 if [ ! -f "JavaDowngrader-Standalone-1.1.2.jar" ]; then
     wget -q https://github.com/RaphiMC/JavaDowngrader/releases/download/v1.1.2/JavaDowngrader-Standalone-1.1.2.jar
@@ -18,3 +19,5 @@ for jarfile in out/cacio-*.jar; do
         java -jar JavaDowngrader-Standalone-1.1.2.jar -v 8 -i "$jarfile" -o "$jarfile.tmp"
         mv "$jarfile.tmp" "$jarfile"
 done
+
+EOF
